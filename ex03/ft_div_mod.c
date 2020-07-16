3690468ab0760282daf474681ff7d87775f7582d
shmalwanJHB <67931188+shmalwanJHB@users.noreply.github.com>
2020-07-16 13:20:27 +0200

    Update ft_div_mod.c
    Function to divide parameters has multiple warnings, with no errors. 
    Exercise committed and pushed..

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -1,22 +1,22 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
-void     ft_div_mod(int a, int b, int *div, int *mod)
-{
+void     ft_div_mod(int a, int b, int *div, int *mod)	{
+	
 	int p = 0;
-	if (p! = '\0')
+	if (p ! = '\0')
 	{
-		*mod = a % b;
-		*div = a / b;
+		*mod = (a % b);
+		*div = (a / b);
 	}
 }
-int main(void)
-{
+int main(void)	{
+	
 	int x;  int a;
 	int y;  int b;
-	x = 6;
-	y = 17;
-  
+	
+	x = 6;	y = 17;
+	
 	ft_div_mod(x, y, &a, &b);
 	printf("x = %i && y = %i\n", x, y)
-		printf("div = %i && mod = %i\n" ,a ,b);
+		printf("div = %i && mod = %i\n", a, b);
 }
